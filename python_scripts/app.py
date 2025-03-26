f2a6018f4e89f646dff5fb8399ca403e447b7d1c
Benjamin Pacyga <142643606+bpacyga@users.noreply.github.com>
2025-03-26 16:05:48 -0400

    Foundation for app.py, create Python folder

---
+++ python_scripts/app.py
@@ -0,0 +1,13 @@
+from tkinter import *
+from tkinter import ttk
+import serial
+
+#initialize TKInter Window
+
+#Establish database connection
+
+#Eastablish Thread for reading serial data
+
+#create_widgets(): Populates TKInter GUI
+
+#exit(): Handles shutdown of serial and database connection
