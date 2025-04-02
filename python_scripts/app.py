cf9152aa273f230effb85b900b8ba3097746888c
Benjamin Pacyga <142643606+bpacyga@users.noreply.github.com>
2025-04-02 15:52:00 -0400

    TKInter Window code in app.py

--- python_scripts/app.py
+++ python_scripts/app.py
@@ -3,11 +3,26 @@ from tkinter import ttk
 import serial
 
 #initialize TKInter Window
+root = Tk()
+frame = ttk.Frame(root, padding=10)
+frame.grid()
+create_widgets()
 
 #Establish database connection
 
-#Eastablish Thread for reading serial data
+#Establish Thread for reading serial data
+
+#Begin Displaying TKInter Window
+root.mainloop()
 
 #create_widgets(): Populates TKInter GUI
+create_widgets():
+  ttk.Label(frm, text="Water Flow Sensor").grid(column=1, row=0)
+  ttk.Label(frame, text="Flow Rate: ").grid(column=0, row=1)
+  ttk.Label(frame, text="Total Volume: ").grid(column=1, row=1)
+  ttk.Label(frame, text="Average Flow Rate: ").grid(column=2, row=1)
+  ttk.Button(frame, text="Quit", command=exit(root)).grid(column=1, row=0)
 
 #exit(): Handles shutdown of serial and database connection
+def exit(root):
+  root.destroy
