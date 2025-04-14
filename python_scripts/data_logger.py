f791c9b21c7dc2ad6085792199496250853153d4
bpacyga <ben@pacyga.com>
2025-04-14 16:20:20 -0400

    Mutex Locks and Window Styling
    Global variables are stored in their own module.
    Mutex Locks are implemented.
    Minor changes to TKInter window

--- python_scripts/data_logger.py
+++ python_scripts/data_logger.py
@@ -1,6 +1,7 @@
 import serial
 import threading as thr
 import time
+import global_vars
 
 #read_data(): retrieves data from Ardiuno, updating variables
 def read_data():
@@ -16,8 +17,11 @@ def read_data():
   return new_data
 
 #update_globals(): once lock is aquired, allows other thread to receive new data
-def update_globals(flowrate, total_vol, avg_flowrate):
-  #use mutex lock, set the variables
+def update_globals(g_lock, flowrate, total_vol, avg_flowrate):
+  with g_lock:
+    global_vars.g_flowrate = flowrate
+    global_vars.g_total_vol = total_vol
+    global_vars.g_avg_flowrate = avg_flowrate
 
 #update_database(): connects to Thingspeak to update it with new data
 def update_database(flowrate, total_vol, avg_flowrate):
@@ -25,14 +29,14 @@ def update_database(flowrate, total_vol, avg_flowrate):
   print(f"{flowrate} {total_vol} {avg_flowrate}")
 
 #Main Thread loop
-def logger_loop():
+def logger_loop(g_lock):
   #open serial port
   t = thr.currentThread()
 
   while(getattr(t, "running", True)):
     new_data = read_data()
-    update_globals(new_data['flowrate'], new_data['total_vol'], new_data['avg_flowrate'])
+    update_globals(g_lock, new_data['flowrate'], new_data['total_vol'], new_data['avg_flowrate'])
     update_database(new_data['flowrate'], new_data['total_vol'], new_data['avg_flowrate'])
-    time.sleep(3) #change sleep seconds as needed
+    time.sleep(1) #change sleep seconds as needed
 
   #close serial port
